733c8b9f9a9d76628db6f2ee9ad2489a3f308234
Daniel Hernandez Torres <daedheto@gmail.com>
2021-12-26 23:08:47 -0300

    Cambios

--- JS/desafioAjax.js
+++
@@ -1,7 +0,0 @@
-let criptos = document.getElementById("criptos");
-fetch("http://hp-api.herokuapp.com/api/characters/students")
-  .then((response) => response.json())
-  .then((datosCripto) => {
-    let criptoArray = Object.entries(datosCripto);
-    console.log(criptoArray);
-  });
